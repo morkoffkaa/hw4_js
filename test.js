9bcb1d23456a25661f614c2cd959aad284352905
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-08 08:59:09 +0200

    add files

---
+++ test.js
@@ -0,0 +1,12 @@
+let a = prompt('Please input the first number:');
+let b = prompt('Please input the second number:');
+
+let add = (Number(a) + Number(b));
+let sub = (Number(a) - Number(b));
+let mult = (Number(a) * Number(b));
+let div = (Number(a) / Number(b));
+
+alert(`${a} + ${b} = ${add}`);
+alert(`${a} - ${b} = ${sub}`);
+alert(`${a} * ${b} = ${mult}`);
+alert(`${a} / ${b} = ${div}`);
