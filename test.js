34cb560b9bd71c42ef63145b43203e8725871b11
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-08 14:55:51 +0200

    change js part

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 let a = prompt('Please input the first number:');
 let b = prompt('Please input the second number:');
 
-let add = (Number(a) + Number(b));
-let sub = (Number(a) - Number(b));
-let mult = (Number(a) * Number(b));
-let div = (Number(a) / Number(b));
+a = Number(a);
+b = Number(b);
 
-alert(`${a} + ${b} = ${add}`);
-alert(`${a} - ${b} = ${sub}`);
-alert(`${a} * ${b} = ${mult}`);
-alert(`${a} / ${b} = ${div}`);
+let add = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+
+alert(`${a} + ${b} = ${add} \n ${a} - ${b} = ${sub} \n ${a} * ${b} = ${mult} \n ${a} / ${b} = ${div}`);
